9637016654167072e50abdc56761be0c878398b5
Oliver <oliverhenrix0@gmail.com>
2026-07-03 16:22:55 +0200

    Koble til Firebase-prosjektet stilvelger-918a9

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Firebase-konfigurasjon for brukerkontoer.
-// Verdiene får du fra Firebase Console -> Prosjektinnstillinger -> Dine apper.
-// (Disse er trygge å dele offentlig - sikkerheten ligger i Firestore-reglene.)
+// (Disse verdiene er trygge å dele offentlig - sikkerheten ligger i
+// Firestore-reglene, som kun lar hver bruker lese/skrive sine egne data.)
 const FIREBASE_CONFIG = {
-  apiKey: "SETT-INN-API-KEY",
-  authDomain: "SETT-INN.firebaseapp.com",
-  projectId: "SETT-INN",
-  storageBucket: "SETT-INN.appspot.com",
-  messagingSenderId: "SETT-INN",
-  appId: "SETT-INN",
+  apiKey: "AIzaSyDbOkZXcUvCWj-Wx-x-hzXDyA1c6ze5tds",
+  authDomain: "stilvelger-918a9.firebaseapp.com",
+  projectId: "stilvelger-918a9",
+  storageBucket: "stilvelger-918a9.firebasestorage.app",
+  messagingSenderId: "2376163082",
+  appId: "1:2376163082:web:3604abd20830d6d5bfe4f3",
 };
